d2d8c1d0751cd35570fbe804d5d26f9f6117d0b0
kehinde <bankolek1@gmail.com>
2025-01-20 15:59:54 +0100

    [any message]

--- app/main.ts
+++ app/main.ts
@@ -3,10 +3,20 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    console.log("Received data:", data.toString());
+  });
+
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.end();
+
   socket.on("close", () => {
-    socket.end();
+    console.log("Client disconnected!");
+  });
+
+  socket.on("error", (err) => {
+    console.error("Socket error:", err.message);
   });
 });
 
